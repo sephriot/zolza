c6d667e7ed28317a920d8bcad3608938b195bb10
Jakub Wójtowicz <wojtjk@gmail.com>
2024-01-11 14:40:14 +0100

    CSS update
    Signed-off-by: Jakub Wójtowicz <wojtjk@gmail.com>

--- src/app/step.tsx
+++ src/app/step.tsx
@@ -13,13 +13,15 @@ export default function Step() {
     return (
         <div className="min-w-full items-center justify-between">
             <div className="items-center mb-8 text-4xl text-center">{current_step_data.title}</div>
-            <div className="mt-12 grid grid-flow-rows grid-cols-3 gap-24 text-xl max-w-full justify-items-center">
+            <div className="mt-12 grid lg:grid-cols-4 md:grid-cols-2 lg:gap-10 md:gap-8 gap-6 text-xl max-w-full justify-items-center">
                 {current_step_data.items.map((element, idx) => {
                     return <div className="max-w-64" key={element.title}>
-                        <input onClick={() => setStepIndex(element.nextStep)} id={idx.toString()} type="checkbox" />
-                        <label className="ml-2 select-none" htmlFor={idx.toString()}>
-                            {element.title}
-                        </label>
+                        <div className="form-group">
+                            <input className="clear-none" onClick={() => setStepIndex(element.nextStep)} id={idx.toString()} type="checkbox" />
+                            <label className="clear-none float-right ml-2 select-none" htmlFor={idx.toString()}>
+                                {element.title}
+                            </label>
+                        </div>
                     </div>;
                 })}
             </div>
